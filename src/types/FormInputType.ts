e4e6db6625f87c3ea6084be6314d2fdcacce3123
Kenty Akoarum <kamome-black@akoarum.net>
2022-08-08 14:10:07 +0900

    add file type

--- src/types/FormInputType.ts
+++ src/types/FormInputType.ts
@@ -1,1 +1,1 @@
-export type FormInputType = 'text' | 'number' | 'email' | 'tel' | 'url' | 'radio' | 'checkbox' | 'text_area'
+export type FormInputType = 'text' | 'number' | 'email' | 'tel' | 'url' | 'radio' | 'checkbox' | 'text_area' | 'file'
