32bac68f39636ecf418d55f469295f2c921e9a2d
Kenty Akoarum <kamome-black@akoarum.net>
2023-07-26 13:03:33 +0900

    add 'select_box' to FormInputType

--- src/types/FormInputType.ts
+++ src/types/FormInputType.ts
@@ -1,1 +1,11 @@
-export type FormInputType = 'text' | 'number' | 'email' | 'tel' | 'url' | 'radio' | 'checkbox' | 'text_area' | 'file'
+export type FormInputType =
+  | 'text'
+  | 'number'
+  | 'email'
+  | 'tel'
+  | 'url'
+  | 'radio'
+  | 'checkbox'
+  | 'text_area'
+  | 'file'
+  | 'select_box'
